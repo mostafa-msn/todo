90c6a231e5188dc7a895d5ca0be1998028406978
mostafa-msn <soltaninejad.msn@gmail.com>
2022-11-01 14:30:20 +0330

    added test

--- task/tests.py
+++ task/tests.py
@@ -1,14 +1,35 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from rest_framework.test import force_authenticate
+from rest_framework.test import APIRequestFactory
+from django.contrib.auth.models import User, AnonymousUser
+from account.models import UserProfile
 from task.models import Task, Project
+from task.views import ProjectListAPI
+
 
 class ProjectTestCase(TestCase):
     def setUp(self):
-        Project.objects.create(name="lion", sound="roar")
-        Project.objects.create(name="cat", sound="meow")
+        # Every test needs access to the request factory.
+        self.factory = APIRequestFactory()
+        # create data
+        self.developer = User.objects.create_user(
+            username="developer", email=None, password="password",
+            first_name="mostafa", last_name="soltani",
+        )
+        self.manager = User.objects.create_user(
+            username="project_manager", email=None, password='password',
+            first_name="mike", last_name="soltani",
+        )
+        UserProfile.objects.create(user=self.developer, role=1)
+        UserProfile.objects.create(user=self.manager, role=2)
+        Project.objects.create(user=self.manager, title="project_1", description="project description")
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Animal.objects.get(name="lion")
-        cat = Animal.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+    def test_details(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/project/list')
+        # Use this syntax for class-based views.
+        force_authenticate(request, user=self.manager)
+        response = ProjectListAPI.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+
